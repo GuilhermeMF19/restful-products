e0f4a54c8f507e9be578fca55eff1a7a2dea99a5
Guilherme <guilherme_riba@hotmail.com>
2024-04-23 10:01:21 -0300

    Create project with SpringBoot

---
+++ src/main/java/com/gmf/RestfulProductsApplication.java
@@ -0,0 +1,13 @@
+package com.gmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmf/RestfulProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
