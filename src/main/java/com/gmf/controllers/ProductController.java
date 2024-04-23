e6d0801b02f5213736867d240734a764b2122a1f
Guilherme <guilherme_riba@hotmail.com>
2024-04-23 14:13:57 -0300

    Implements HATEOAS

--- src/main/java/com/gmf/controllers/ProductController.java
+++ src/main/java/com/gmf/controllers/ProductController.java
@@ -22,6 +22,8 @@ import com.gmf.repository.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -38,7 +40,13 @@ public class ProductController {
 	
 	@GetMapping("/products")
 	public ResponseEntity<List<ProductModel>> getAllProducts() {
-		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+		List<ProductModel> productsList = productRepository.findAll();
+		if(!productsList.isEmpty()) {
+			for (ProductModel product : productsList) {
+				product.add(linkTo(methodOn(ProductController.class).getOneProduct(product.getIdProduct())).withSelfRel());
+			}
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(productsList);
 	}
 	
 	@GetMapping("/products/{id}")
@@ -47,6 +55,7 @@ public class ProductController {
 		if(productO.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 		}
+		productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Proucts List"));
 		return ResponseEntity.status(HttpStatus.OK).body(productO.get());
 	}
 	
